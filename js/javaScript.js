93d7c8c267150fea283b899193fb095c06699d06
Kevin Alec Neri Lazzarotto <kevin.lazzarotto@hotmail.com>
2020-09-06 17:53:36 -0300

    Implementacao de logica, mudanca de escopo do projeto, para um auto complit de um buscador de enderecos por meio do CEP, insercao de estilos e ajustes de interface

---
+++ js/javaScript.js
@@ -0,0 +1,54 @@
+var cepRecebido;
+
+var bairro = document.querySelector('#bairro')
+var cep = document.querySelector('#cep')
+var localidade = document.querySelector('#localidade')
+var logradouro = document.querySelector('#logradouro')
+var uf = document.querySelector('#uf')
+
+//CAPITURANDO VALOR INSERIDO NO INPUT E ARMAZENANDO EM UMA VARIAVEL
+document.querySelector("#numero").addEventListener("focusout", recebedorDados);
+
+function recebedorDados(){
+    //NAO PODE SER parseInt, POIS DESSA FORMA O VALOR SERA CONVERTIDO CASO COMESSE COM '0' 
+    var valorRecebido = document.getElementById('numero').value;
+
+    //RETIRANDO "-" DO CEP CASO TENHA
+    var verificarCep = valorRecebido.replace("-", "");
+
+    //ATRIBUINDO O VALOR VERIFICADO DO CEP, AO PROPRIO CEP
+    cepRecebido = verificarCep
+    
+    console.log("Quantidade: "+valorRecebido);
+    console.log("Dias: "+cepRecebido);
+
+    acessoBancoDados();
+}
+
+//LOCALIZAR VALOR DE REQUISICAO DENTRO DO .JSON
+function acessoBancoDados(){
+    console.log("acessando FETCH, o valor do CEP foi: "+ cepRecebido)
+
+    try {
+        fetch(`http://viacep.com.br/ws/${cepRecebido}/json/`)
+        .then(function (response) {
+            return response.json()
+
+        })
+        .then(function (response){
+            bairro.innerHTML = response.bairro
+            cep.innerHTML = response.cep
+            localidade.innerHTML = response.localidade
+            logradouro.innerHTML = response.logradouro
+            uf.innerHTML = response.uf
+            
+            return response
+            
+       })
+            
+    } catch (error) {
+        console.log("Nao foi localizada a requisicao no servidor, tente novamente"+ error);
+        
+    }
+        
+}
